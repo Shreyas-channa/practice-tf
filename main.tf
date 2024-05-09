c3d55b04d236985e0d90f6938e67297ad362afac
table <table@gmail.com>
2024-05-09 18:51:27 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,8 +1,15 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+resource "aws-instance " "my-instance" {
+    ami = "ami-07caf09b362be10b8"
     instance_type = "t2.micro"
+    key_name = "ak-virginia"
+    tags = {
+        env = "dev"
+        name = "instance-1"
+    }
+    vpc_security_group_ids = ["sg-05ae5ce65f18d0610", "sg-023e1d359bd5464cd"]
+  
 }
